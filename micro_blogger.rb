f9aee8528160a40d01f511bb12fc95f36872dafc
Chris Cotton <chris.cottonn@gmail.com>
2015-03-28 13:32:19 -0700

    I2: A Better Interface

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -15,9 +15,25 @@ class MicroBlogger
       puts 'Tweet must be no more than 140 characters.'
     end
   end
+
+  def run
+    puts 'Welcome to the JSL Twitter Client!'
+
+    command = ''
+    while command != 'q'
+      printf 'enter command: '
+      input = gets.chomp
+      parts = input.split
+      command = parts[0]
+
+      case command
+      when 'q' then puts 'Goodbye!'
+      when 't' then tweet(parts[1..-1].join(' '))
+      else puts "Sorry, I don't know how to #{command}"
+      end
+    end
+  end
 end
 
 blogger = MicroBlogger.new
-test = "".ljust(140, "abcd")
-blogger.tweet(test)
-blogger.tweet(test << '1')
+blogger.run
