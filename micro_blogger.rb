b1b3ebceeadf60d8189236db5c6574c186d4f32e
Chris Cotton <chris.cottonn@gmail.com>
2015-03-28 13:22:13 -0700

    I1: Posting Tweets

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -7,4 +7,17 @@ class MicroBlogger
     puts 'Initializing...'
     @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+    if message.length <= 140
+      @client.update(message)
+    else
+      puts 'Tweet must be no more than 140 characters.'
+    end
+  end
 end
+
+blogger = MicroBlogger.new
+test = "".ljust(140, "abcd")
+blogger.tweet(test)
+blogger.tweet(test << '1')
