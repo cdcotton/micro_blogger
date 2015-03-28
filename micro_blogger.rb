0967aa3eaeacb07449f579feba87a001e78236f3
Chris Cotton <chris.cottonn@gmail.com>
2015-03-28 13:52:03 -0700

    I3: Send Direct Messages

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -16,6 +16,31 @@ class MicroBlogger
     end
   end
 
+  def followers_list
+    screen_names = []
+    @client.followers.each do |follower|
+      screen_names << @client.user(follower).screen_name
+    end
+    screen_names
+  end
+
+  def dm(target, message)
+    if followers_list.include?(target)
+      puts "Trying to send #{target} this direct message:"
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts 'You can only DM people who follow you.'
+    end
+  end
+
+  def spam_my_followers(message)
+    followers_list.each do |follower|
+      dm(follower, message)
+    end
+  end
+
   def run
     puts 'Welcome to the JSL Twitter Client!'
 
@@ -29,6 +54,8 @@ class MicroBlogger
       case command
       when 'q' then puts 'Goodbye!'
       when 't' then tweet(parts[1..-1].join(' '))
+      when 'dm' then dm(parts[1], parts[2..-1].join(' '))
+      when 'spam' then spam_my_followers(parts[1..-1].join(' '))
       else puts "Sorry, I don't know how to #{command}"
       end
     end
